1438bf34a27c72795cabb9fff1e1e8094f97a698
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2021-01-02 18:21:27 +0100

    Enabling the buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 
+import 'timer.dart';
+import 'timermodel.dart';
 import 'widgets.dart';
 
 void main() => runApp(MyApp());
@@ -20,79 +22,91 @@ class MyApp extends StatelessWidget {
 
 class TimerHomePage extends StatelessWidget {
   final double defaultPadding = 5.0;
+  final CountDownTimer timer = CountDownTimer();
 
   @override
   Widget build(BuildContext context) {
+    timer.startWork();
     return Scaffold(
         appBar: AppBar(
           title: Text('My Work Timer'),
         ),
         body: LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints) {
-          final double availableWidth = constraints.maxWidth;
-          return Column(children: [
-            Row(
-              children: [
-                Padding(padding: EdgeInsets.all(defaultPadding)),
-                Expanded(
-                  child: ProductivityButton(
-                    color: Color(0xff009688),
-                    text: 'Work',
-                    onPressed: emptyMethod,
+          builder: (BuildContext context, BoxConstraints constraints) {
+            final double availableWidth = constraints.maxWidth;
+            return Column(children: [
+              Row(
+                children: [
+                  Padding(padding: EdgeInsets.all(defaultPadding)),
+                  Expanded(
+                    child: ProductivityButton(
+                      color: Color(0xff009688),
+                      text: 'Work',
+                      onPressed: () => timer.startWork(),
+                    ),
                   ),
-                ),
-                Padding(padding: EdgeInsets.all(defaultPadding)),
-                Expanded(
-                  child: ProductivityButton(
-                    color: Color(0xff607D8B),
-                    text: 'Short Break',
-                    onPressed: emptyMethod,
+                  Padding(padding: EdgeInsets.all(defaultPadding)),
+                  Expanded(
+                    child: ProductivityButton(
+                      color: Color(0xff607D8B),
+                      text: 'Short Break',
+                      onPressed: () => timer.startBreak(true),
+                    ),
                   ),
-                ),
-                Padding(padding: EdgeInsets.all(defaultPadding)),
-                Expanded(
-                  child: ProductivityButton(
-                    color: Color(0xff455A64),
-                    text: 'Long Break',
-                    onPressed: emptyMethod,
+                  Padding(padding: EdgeInsets.all(defaultPadding)),
+                  Expanded(
+                    child: ProductivityButton(
+                      color: Color(0xff455A64),
+                      text: 'Long Break',
+                      onPressed: () =>  timer.startBreak(false),
+                    ),
                   ),
-                ),
-                Padding(padding: EdgeInsets.all(defaultPadding)),
-              ],
-            ),
-            Expanded(
-              child: CircularPercentIndicator(
-                radius: availableWidth / 2,
-                lineWidth: 10,
-                percent: 1,
-                center:
-                    Text("30:00", style: Theme.of(context).textTheme.headline4),
-                progressColor: Color(0xff009688),
+                  Padding(padding: EdgeInsets.all(defaultPadding)),
+                ],
               ),
-            ),
-            Row(
-              children: [
-                Padding(padding: EdgeInsets.all(defaultPadding)),
-                Expanded(
-                  child: ProductivityButton(
-                    color: Color(0xff212121),
-                    text: 'Stop',
-                    onPressed: emptyMethod,
+              Expanded(
+                child: StreamBuilder(
+                    initialData: '00:00',
+                    stream: timer.stream(),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      TimerModel timer = (snapshot.data == '00:00')
+                          ? TimerModel('00:00', 1)
+                          : snapshot.data;
+                      return (Expanded(
+                          child: CircularPercentIndicator(
+                        radius: availableWidth / 2,
+                        lineWidth: 10,
+                        percent: timer.percent,
+                        center: Text(timer.time + ':00',
+                            style: Theme.of(context).textTheme.headline4),
+                        progressColor: Color(0xff009688),
+                      )));
+                    }),
+              ),
+              Row(
+                children: [
+                  Padding(padding: EdgeInsets.all(defaultPadding)),
+                  Expanded(
+                    child: ProductivityButton(
+                      color: Color(0xff212121),
+                      text: 'Stop',
+                      onPressed: () => timer.stopTimer(),
+                    ),
                   ),
-                ),
-                Padding(padding: EdgeInsets.all(defaultPadding)),
-                Expanded(
-                  child: ProductivityButton(
-                    color: Color(0xff009688),
-                    text: 'Restart',
-                    onPressed: emptyMethod,
+                  Padding(padding: EdgeInsets.all(defaultPadding)),
+                  Expanded(
+                    child: ProductivityButton(
+                      color: Color(0xff009688),
+                      text: 'Restart',
+                      onPressed: () => timer.startTimer(),
+                    ),
                   ),
-                ),
-                Padding(padding: EdgeInsets.all(defaultPadding)),
-              ],
-            ),
-          ]);
-        }));
+                  Padding(padding: EdgeInsets.all(defaultPadding)),
+                ],
+              ),
+            ]);
+          },
+        ));
   }
 
   void emptyMethod() {}
